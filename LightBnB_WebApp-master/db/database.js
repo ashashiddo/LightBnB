15838481566dd183348ef60d755262684fa5f95c
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-29 09:46:58 -0400

    Refactor getAllProperties function to query the PostgreSQL database instead of using JSON data

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,15 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
+const { Pool } = require('pg');
+const properties = require('./json/properties.json'); // Import properties data
+const users = require('./json/users.json'); // Import users data
+
+
+const pool = new Pool({
+  user: 'yourusername',
+  host: 'your_host',
+  database: 'your_database',
+  password: 'your_password',
+  port: 5432, // replace with your database port if necessary
+});
 
 /// Users
 
@@ -8,12 +18,13 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
+const getUserWithEmail = function(email) {
   let resolvedUser = null;
   for (const userId in users) {
     const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
+    if (user && user.email && user.email.toLowerCase() === email.toLowerCase()) {
       resolvedUser = user;
+      break;
     }
   }
   return Promise.resolve(resolvedUser);
@@ -24,7 +35,7 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
+const getUserWithId = function(id) {
   return Promise.resolve(users[id]);
 };
 
@@ -33,7 +44,7 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
+const addUser = function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -47,7 +58,7 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
+const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
 };
 
@@ -59,12 +70,25 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+        
+  let queryString = `
+      SELECT *
+      FROM properties
+      LIMIT $1
+    `;
+        
+  queryParams.push(limit);
+        
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
 };
 
 /**
@@ -72,7 +96,7 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
+const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
